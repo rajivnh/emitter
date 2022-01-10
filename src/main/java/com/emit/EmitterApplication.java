9f1f3bd686bd4dd0d8c7b94aa4b89a364ed95051
Rajiv Kumbhakar <rajivnh@yahoo.com>
2022-01-10 11:50:19 +0530

    Initial Commit

--- src/main/java/com/emit/EmitterApplication.java
+++ src/main/java/com/emit/EmitterApplication.java
@@ -15,51 +15,51 @@ import reactor.core.publisher.Sinks.EmitResult;
 @RestController
 @SpringBootApplication
 public class EmitterApplication {
-    private Sinks.Many<Employee> sink;
+	private Sinks.Many<Employee> sink;
 
-    public EmitterApplication() {
-    	this.sink = Sinks.many().multicast().onBackpressureBuffer();
-    }
-    
+	public EmitterApplication() {
+		this.sink = Sinks.many().multicast().onBackpressureBuffer();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(EmitterApplication.class, args);
 	}
-	
-    @GetMapping("/send")
-    public void test() throws InterruptedException {
-    	Employee[] emps = new Employee[60];
-    	
-    	for(int i=0;i<60;i++) {
-    		emps[i] = new Employee();
-    		
-    		emps[i].setId(String.valueOf(i));
-    		
-    		EmitResult result = sink.tryEmitNext(emps[i]);
 
-    		if(result.isFailure()) {
-    			System.out.println("FAILED");
-    		}
-    		
-    		Thread.sleep(200);
-    	}
+	@GetMapping("/send")
+	public void test() throws InterruptedException {
+		Employee[] emps = new Employee[60];
+
+		for (int i = 0; i < 60; i++) {
+			emps[i] = new Employee();
+
+			emps[i].setId(String.valueOf(i));
+
+			EmitResult result = sink.tryEmitNext(emps[i]);
+
+			if (result.isFailure()) {
+				System.out.println("FAILED");
+			}
+
+			Thread.sleep(200);
+		}
 	}
-    
-    @GetMapping("/complete")
-    public void complete() {
-        sink.tryEmitComplete();
-    }
-    
-    @RequestMapping(path = "/receive", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ServerSentEvent<Employee>> sse() {
-        return sink.asFlux().map(e -> {return ServerSentEvent.builder(e).build();});
-    }
+
+	@GetMapping("/complete")
+	public void complete() {
+		sink.tryEmitComplete();
+	}
+
+	@RequestMapping(path = "/receive", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<ServerSentEvent<Employee>> sse() {
+		return sink.asFlux().map(e -> ServerSentEvent.builder(e).build());
+	}
 }
 
 class Employee {
 	private String id;
-	
+
 	public Employee() {
-		
+
 	}
 
 	public String getId() {
--- src/test/java/com/emit/EmitterApplicationTests.java
+++ src/test/java/com/emit/EmitterApplicationTests.java
@@ -9,5 +9,4 @@ class EmitterApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
